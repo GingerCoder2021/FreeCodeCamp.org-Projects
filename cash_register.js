6558580e2cbe66c33fa50aaa7c6a492bc10305af
Jeremy Duncan <jduncan46@my.devry.edu>
2022-02-20 10:05:46 -0500

    updated test cases and formatting

--- cash_register.js
+++ cash_register.js
@@ -38,6 +38,7 @@ function assertEqual(actual, expected, testName) {
 
 // test cases ==============================================
 
+// Test >=> 1
 var expected1 = { status: "OPEN", change: [["QUARTER", 0.5]] };
 var output1 = checkCashRegister(19.5, 20, [
   ["PENNY", 1.01],
@@ -49,10 +50,22 @@ var output1 = checkCashRegister(19.5, 20, [
   ["TEN", 20],
   ["TWENTY", 60],
   ["ONE HUNDRED", 100],
-]); //should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+]); // should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
 var test1 = assertEqual(output1, expected1, "Function Test: 1");
 
-var expected2 =
+// Test >=> 2
+var expected2 = {
+  status: "OPEN",
+  change: [
+    ["TWENTY", 60],
+    ["TEN", 20],
+    ["FIVE", 15],
+    ["ONE", 1],
+    ["QUARTER", 0.5],
+    ["DIME", 0.2],
+    ["PENNY", 0.04],
+  ],
+};
 var output2 = checkCashRegister(3.26, 100, [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -63,10 +76,11 @@ var output2 = checkCashRegister(3.26, 100, [
   ["TEN", 20],
   ["TWENTY", 60],
   ["ONE HUNDRED", 100],
-]); //should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+]); // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
 var test2 = assertEqual(output2, expected2, "Function Test: 2");
 
-var expected3 =
+// Test >=> 3
+var expected3 = { status: "INSUFFICIENT_FUNDS", change: [] };
 var output3 = checkCashRegister(19.5, 20, [
   ["PENNY", 0.01],
   ["NICKEL", 0],
@@ -77,10 +91,11 @@ var output3 = checkCashRegister(19.5, 20, [
   ["TEN", 0],
   ["TWENTY", 0],
   ["ONE HUNDRED", 0],
-]); //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+]); // should return {status: "INSUFFICIENT_FUNDS", change: []}.
 var test3 = assertEqual(output3, expected3, "Function Test: 3");
 
-var expected4 =
+// Test >=> 4
+var expected4 = { status: "INSUFFICIENT_FUNDS", change: [] };
 var output4 = checkCashRegister(19.5, 20, [
   ["PENNY", 0.01],
   ["NICKEL", 0],
@@ -91,10 +106,24 @@ var output4 = checkCashRegister(19.5, 20, [
   ["TEN", 0],
   ["TWENTY", 0],
   ["ONE HUNDRED", 0],
-]); //should return {status: "INSUFFICIENT_FUNDS", change: []}.
+]); // should return {status: "INSUFFICIENT_FUNDS", change: []}.
 var test4 = assertEqual(output4, expected4, "Function Test: 4");
 
-var expected5 =
+// Test >=> 5
+var expected5 = {
+  status: "CLOSED",
+  change: [
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0],
+  ],
+};
 var output5 = checkCashRegister(19.5, 20, [
   ["PENNY", 0.5],
   ["NICKEL", 0],
@@ -105,5 +134,5 @@ var output5 = checkCashRegister(19.5, 20, [
   ["TEN", 0],
   ["TWENTY", 0],
   ["ONE HUNDRED", 0],
-]); //should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+]); // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
 var test5 = assertEqual(output5, expected5, "Function Test: 5");
