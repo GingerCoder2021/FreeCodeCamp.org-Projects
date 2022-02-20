12d30bfec10cdc8408949d5cf9a81a5a5ebcf502
Jeremy Duncan <jduncan46@my.devry.edu>
2022-02-20 18:53:12 -0500

    updated code

--- cash_register.js
+++ cash_register.js
@@ -15,59 +15,24 @@ Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins 
 
 // function ================================================
 
-//changeObj = { status: "value", change: [value1, value2, value3] };
-
 function checkCashRegister(price, cash, cid) {
-  //cid == "cash in drawer"
-  var changeObj = {};
+  var change = {};
+  var changeDue = (cash - price) * 100;
+  var cidTotal = 0;
 
-  if (price < cash){
-    changeObj.status = "INSUFFICIENT_FUNDS";
-    changeObj.change = [];
-    console.log("|| changeObj", changeObj);
-
-    return changeObj;
-    
-
+  for (var i = 0; i < cid.length; i++) {
+    cidTotal += cid[i][1] * 100;
+  }
+  if (changeDue == cidTotal) {
+    change.status = "CLOSED";
+    change.change = cid;
+    return change;
   }
 
- 
-  return changeObj;
   
-  // {status: "INSUFFICIENT_FUNDS", change: []} or
-  // {status: "CLOSED", change: [...]} or
-  // {status: "OPEN", change: [...]}
-}
-
-
-
-
-
-
-
-
-// assert function =========================================
-function assertEqual(actual, expected, testName) {
-  actual = JSON.stringify(actual);
-  expected = JSON.stringify(expected);
-  
-  if (actual === expected) {
-    console.log("PASSED");
-  } else {
-    console.log(
-      "FAILED: " +
-        testName +
-        ", expected " +
-        expected +
-        ", but got " +
-        actual +
-        "."
-    );
-  }
-}
 
 // test cases ==============================================
-/*
+
 // Test >=> 1
 var expected1 = { status: "OPEN", change: [["QUARTER", 0.5]] };
 var output1 = checkCashRegister(19.5, 20, [
@@ -108,7 +73,7 @@ var output2 = checkCashRegister(3.26, 100, [
   ["ONE HUNDRED", 100],
 ]); // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
 var test2 = assertEqual(output2, expected2, "Function Test: 2");
-*/
+
 // Test >=> 3
 var expected3 = { status: "INSUFFICIENT_FUNDS", change: [] };
 var output3 = checkCashRegister(19.5, 20, [
@@ -123,10 +88,10 @@ var output3 = checkCashRegister(19.5, 20, [
   ["ONE HUNDRED", 0],
 ]); // should return {status: "INSUFFICIENT_FUNDS", change: []}.
 var test3 = assertEqual(output3, expected3, "Function Test: 3");
-/*
+
 // Test >=> 4
 var expected4 = { status: "INSUFFICIENT_FUNDS", change: [] };
-var output4 = checkCashRegister(19.5, 20, [
+var output4 = checkCashRegister(25, 20, [
   ["PENNY", 0.01],
   ["NICKEL", 0],
   ["DIME", 0],
@@ -138,7 +103,7 @@ var output4 = checkCashRegister(19.5, 20, [
   ["ONE HUNDRED", 0],
 ]); // should return {status: "INSUFFICIENT_FUNDS", change: []}.
 var test4 = assertEqual(output4, expected4, "Function Test: 4");
-
+*/
 // Test >=> 5
 var expected5 = {
   status: "CLOSED",
@@ -166,4 +131,3 @@ var output5 = checkCashRegister(19.5, 20, [
   ["ONE HUNDRED", 0],
 ]); // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
 var test5 = assertEqual(output5, expected5, "Function Test: 5");
-*/
